df3a6dd417c132522b8d15f9131895129eb80d43
alm191 <alm191@alu.ua.es>
2023-07-03 23:08:32 +0200

    métodos iniciales .h

---
+++ src/cargs.c
@@ -0,0 +1,19 @@
+// Argument parser library in C
+
+#include "cargs.h"
+
+void cargs_set_identificator(const char identificator) {
+    cargs_identificator = identificator;
+}
+
+void cargs_set_boolean_args(const u_int32_t count, const char* arg_letters) {
+    // TODO
+}
+
+void cargs_associate_extended(const char** ext_names, const char* arg_letters) {
+    // TODO
+}
+
+void cargs_set_data_args(const u_int32_t count, const char* arg_letters) {
+    // TODO
+}
