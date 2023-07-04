adeed9150795dbcf1bb8c392378be9d36ffe9e61
JuanmaDevG <jmgmgonzalez57@gmail.com>
2023-07-04 14:17:25 +0200

    Init functions that set boolean arguments and data arguments

--- include/cargs.h
+++ include/cargs.h
@@ -6,24 +6,27 @@
 #include <stdlib.h>
 #include <stdint.h>     //To have system types (like u_int16_t) include sys/types.h
 #include <stdbool.h>
+#include <string.h>
 
 
-typedef struct ArgPackage {
+typedef struct ArgPackage{
     uint32_t count;
+    //Pointer to first argv value position
     char** values;
 } ArgPackage;
 
 typedef struct ArgPackageVec {
     size_t size;
-    ArgPackage* arg_pckgs;
+    ArgPackage* packages;
 } ArgPackageVec;
+
 
 // Sets the identificator character for arguments. If this function is 
 // not called, by default is '-' like in unix-like systems.
 void cargs_set_identificator(const char new_identificator);
 
 // Sets how many boolean (existent or non existent) args and each_one's letter.
-void cargs_set_boolean_args(const uint32_t count, const char* arg_letters);
+void cargs_set_boolean_args(const char* arg_letters);
 
 // Associates an extended version of an argument letter to make the argument 
 // parsing more verbose.
@@ -31,7 +34,7 @@ void cargs_associate_extended(const char** ext_names, const char* arg_letters);
 
 // Arguments that require data, so their extra arguments data will be 
 // grouped.
-void cargs_set_data_args(const uint32_t count, const char* arg_letters);
+void cargs_set_data_args(const char* arg_letters);
 
 
 #endif // CARGS_H
--- src/cargs.c
+++ src/cargs.c
@@ -1,29 +1,43 @@
-// Argument parser library in C
-
 #include "cargs.h"
 
-static char identificator = '-';
-static char* boolean_args = NULL;
-static char* data_args = NULL;
 
-void cargs_set_identificator(const char new_identificator) {
-    identificator = new_identificator;
-}
+static char arg_id = '-';
 
-void cargs_set_boolean_args(const uint32_t count, const char* arg_letters) {
-    // "abcd" ;  'a', 'b', 'c', 'd'
-    boolean_args = (char*)malloc(count * sizeof(char));
-    // for (int i = 0; i < count; i++) {
-    //     boolean_args[i] = arg_letters[i];
-    // }
-    memcpy(boolean_args, arg_letters, count * sizeof(char));
+//Boolean arguments definitions
+static size_t _bool_args_count = 0;
+static char* _bool_args = NULL;
+static uint8_t* _bool_args_bit_vec = NULL;
 
+//Data arguments definitions
+static char* _data_args;
+static ArgPackageVec _data_packs;
+
+
+void cargs_set_identificator(const char new_id) { arg_id = new_id; }
+
+//Makes the boolean args bit vector
+void cargs_set_boolean_args(const char* arg_letters) {
+    if(!arg_letters) return;
+    _bool_args_count = strlen(arg_letters);
+    
+    _bool_args = (char*)malloc(_bool_args_count);
+    memcpy(_bool_args, arg_letters, _bool_args_count * sizeof(char));
+
+    //Calculating the bits for bit boolean vector
+    _bool_args_bit_vec = (uint8_t*)malloc(
+        (_bool_args_count >> 3) + (size_t)(_bool_args_count & (size_t)0b111 == 0 ? 0 : 1)
+    );
 }
 
 void cargs_associate_extended(const char** ext_names, const char* arg_letters) {
     // TODO
 }
 
-void cargs_set_data_args(const uint32_t count, const char* arg_letters) {
-    // TODO
+void cargs_set_data_args(const char* arg_letters) {
+    if(!arg_letters) return;
+    _data_packs.size = strlen(arg_letters);
+
+    _data_args = (char*)malloc(_data_packs.size);
+    memcpy(_data_args, arg_letters, _data_packs.size);
+    _data_packs.packages = (ArgPackage*)malloc(_data_packs.size * sizeof(ArgPackage));
 }
