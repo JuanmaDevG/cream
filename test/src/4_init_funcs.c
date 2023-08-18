f0878e5cd6bd01976df6d13339009cbc847e804a
Juan Manuel <jmgmgonzalez57@gmail.com>
2023-08-18 15:07:57 +0200

    Solved test error 4

--- test/src/4_init_funcs.c
+++ test/src/4_init_funcs.c
@@ -2,15 +2,18 @@
 
 int main()
 {
-    cargs_set_args(NULL, "abc");
-    printf("Set arguments please\n"); //MOD
+    cargs_set_args(NULL, "abck");
     cargs_set_minimum_data("hbcd", 1, 2, 3, 43, 123);
     cargs_set_maximum_data("hbcd", 2, 5, 7, 21, 54);
 
+    assert(_cargs_minimum_data[0] == 0);
+    assert(_cargs_maximum_data[0] == 0);
     assert(_cargs_minimum_data[1] == 1);
     assert(_cargs_maximum_data[1] == 2);
     assert(_cargs_minimum_data[2] == 2);
     assert(_cargs_maximum_data[2] == 5);
+    assert(_cargs_minimum_data[3] == 0);
+    assert(_cargs_maximum_data[3] == 0);
 
     finish(4, "init functions");
 }
---
+++ test/src/5_init_funcs.c
@@ -0,0 +1,16 @@
+#include "init_funcs_test.h"
+
+/*
+    Putting conditions and loading the arguments
+    Testing the functions the hard as possible
+*/
+
+int tmp_argc = 0;
+char* tmp_argv[] = {
+    "program_name"
+};
+
+int main()
+{
+
+}
