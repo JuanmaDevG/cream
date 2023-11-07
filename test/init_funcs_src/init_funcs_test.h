19c3bba04546a3fb96e6644e9b596fbff3e5109e
Juan Manuel González <juanmauniversidad@outlook.es>
2023-11-07 22:53:01 +0100

    Changed filename tests to make them some more descriptive and erased batch test files. In progress moving the testing to ctest

--- test/init_funcs_src/init_funcs_test.h
+++ test/init_funcs_src/init_funcs_test.h
@@ -4,7 +4,4 @@
 
 const char* finish_msg = "Finished test %d from %s\n";
 
-//Some more needed data...
-
-
 void finish(uint32_t test_id, const char* test_name) { printf(finish_msg, test_id, test_name); }
