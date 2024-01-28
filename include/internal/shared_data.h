aaa527b23b28a82e708718cf6b693edf534dd762
JuanmaDevG <jmgmgonzalez57@gmail.com>
2024-01-29 00:39:28 +0100

    Thinking about how to restructure the library to give support for utf-8 and utf-16. I may think about a hash table but is challenging that the solution does not require two memory blocks instead of one. I would prefer just to use one mem block...

--- include/internal/shared_data.h
+++ include/internal/shared_data.h
@@ -8,13 +8,15 @@
 typedef struct _cream_data_package {
     uint32_t count;
     char** values;                  //Pointer to first data value position in argv
-    uint8_t inline_data_offset;     //If the argument's data is inlined, this is the offset in characters (like with -L/my/libdir/ offset will be 2 [-, L])
+    uint8_t inline_data_offset;     //Represents the offset if the argument's data is inside the argument option (-LsomeDirectory)
 } _cream_data_package;
 
 typedef struct _cream_option_data {
-    uint8_t maximum_data_count;         //Maximum data pieces supported
-    uint8_t minimum_data_count;         //Minimum data pieces supported
-    uint32_t actual_data_count;         //Actual string data pieces that the argument contains (could be many more for argument options with no declared limits)
+    uint32_t max_data_peices;           //Absolute maximum data pieces for this argument option (cannot surpass)
+    uint32_t min_data_pieces;           //Absolute minimum data pieces for this argument option (must be major or equal)
+    uint8_t max_data_per_call;          //Maximum data pieces per argument option call
+    uint8_t min_data_per_call;          //Minimum data pieces per argument option call
+    uint32_t actual_data_count;         //Actual string data pieces that the argument has associated
     _expandable_stack data;             //Contains the _cream_data_packages and can be extracted with the expandable stack functions
     char** data_relocation_buffer;      //If not NULL, means that the expandable stack has been erased to move all the data string pointers to this buffer
 } _cream_option_data;
@@ -24,7 +26,7 @@ typedef struct _cream_argument {
     bool cannot_be_repeated;                //If false and has been used, the error system will interpret the argument repetition as an error
     bool has_been_used_already;             //If true, the error system will offer the possibility to interpret it's repetition as an error
     _cream_option_data* data_container;     //Won't be null if the argument is a data argument
-    char* extended_version;                 //Pointer to extended version: {-f -> --force} (without '-', that's only the id, sintactically double for extended)
+    char* extended_version;                 //Pointer to extended version: {-f -> --force} (does not contain argument option id like '-')
 } _cream_argument;
 
 
@@ -34,17 +36,12 @@ typedef struct _cream_argument {
     ------------------------
 */
 
-extern char _arg_id;
-
-#define INVALID_CHARS 33
-#define INVALID_CHARS_FRONT 127
-#define ASCII_TABLE_SIZE (256 - INVALID_CHARS - INVALID_CHARS_FRONT)
+extern const char* _cream_arg_id; //TODO: redefine (and support for multiple arg ids)
 
 extern _expandable_stack _cream_general_buffer;                         //A buffer for general usage data
 
-//TODO: complete this
-extern uint8_t CREAM_BOTTOM_LIMIT_CHAR; //MOD
-extern uint8_t CREAM_TOP_LIMIT_CHAR;
+//TODO: think about to change to a hash board or to maintain the indexed vector structure
+// A hash table may be better for charset compatibility and maintainability, but a bit less performant
 extern _cream_argument** _cream_valid_arg_options;                       //A list of independent pointers to the memory block that contains the information from available arguments
 
 extern char* _cream_declared_option_chars;                              //Points to the declared options (first bool options, then data options)
