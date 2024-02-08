0acdcc713d4ce2137066da942fb0b06126f6303e
JuanmaDevG <jmgmgonzalez57@gmail.com>
2024-02-08 22:58:09 +0100

    Some inlined docuemntation and some more minmal changes, still refactoring

--- src/shared_data/shared_data.c
+++ src/shared_data/shared_data.c
@@ -2,42 +2,15 @@
 
 char _arg_id = '-';
 
-/*
-    ----------------------
-    General purpose buffer
-    ----------------------
-*/
-
 _expandable_stack _cream_general_buffer = {{0}, NULL, 0, 0, NULL, NULL};
-
-
-/*
-    ----------------------------
-    Argument options information
-    ----------------------------
-*/
 
 char* _cream_declared_arg_option_chars = NULL;
 size_t _cream_option_count = 0;
 _cream_argument* _cream_declared_arg_options = NULL;
 _cream_option_data* _cream_declared_options_data = NULL;
-_cream_argument* _cream_valid_arg_options[ASCII_TABLE_SIZE] = {NULL};
+_cream_argument** _cream_valid_arg_options = NULL;
 
-
-/*
-    ------------------
-    Extended arguments
-    ------------------
-*/
-
-size_t _cream_ext_arg_count = 0;
-
-
-/*
-    ----------------------------------
-    Anonymous argument storage buffers
-    ----------------------------------
-*/
+size_t _cream_extended_arg_count = 0;
 
 size_t _cream_anon_arg_count = 0;
 _expandable_stack _cream_anonymous_args = {{0}, NULL, 0, 0, NULL, NULL};
--- src/shared_data/shared_data.h
+++ src/shared_data/shared_data.h
@@ -36,21 +36,23 @@ typedef struct _cream_argument {
     ------------------------
 */
 
-extern const char* _cream_arg_id; //TODO: redefine (and support for multiple arg ids)
+extern char _cream_back_arg_id_limit;                           // Argument identificator with lowest value
+extern char _cream_front_arg_id_limit;                          // Argument identificator with highest value
+extern const uint8_t* _cream_arg_identificators;                // Bit vector indicating the existence of an argument option
 
-extern _expandable_stack _cream_general_buffer;                 //A buffer for general usage data
+extern _expandable_stack _cream_general_buffer;                 // A buffer for general usage data
 
-extern const char cream_limit_back_char;                        //Character from the arg option input with lowest value
-extern const char cream_limit_back_char;                        //Character from the arg option input with the highest value
-extern _cream_argument** _cream_valid_arg_options;              //A list of independent pointers to the memory block that contains the information from available arguments
+extern const char _cream_back_arg_opt_limit;                    // Argument option character with lowest value
+extern const char _cream_front_arg_opt_limit;                   // Argument option character with highest value
+extern _cream_argument** _cream_valid_arg_options;              // A table of pointers (wide as char limits) to the memory block that contains the information from available arguments
 
-extern char* _cream_declared_option_chars;                      //Points to the declared options (first bool options, then data options)
-extern uint32_t _cream_option_count;                            //Counts the number of argument options
-extern _cream_argument* _cream_declared_arg_options;            //The memory block that contains the available argument options information
-extern _cream_option_data* _cream_declared_options_data;        //The memory block that contains the input data from the available data argument options
+extern char* _cream_declared_option_chars;                      // Stores the argument option chars declared (not searched sequentially, just for some internal checking)
+extern uint32_t _cream_option_count;                            // Total argument option count
+extern _cream_argument* _cream_declared_arg_options;            // Contains the available argument options information
+extern _cream_option_data* _cream_declared_options_data;        // Contains the input data received by the corresponding data argument options
 
-extern size_t _cream_ext_arg_count;                             //Stores the extended argument count
+extern size_t _cream_extended_arg_count;                             // Stores the extended argument count
 
-extern size_t _cream_anon_arg_count;                            //Contains the total number of anonymous arguments
-extern _expandable_stack _cream_anonymous_args;                 //Contains _cream_data_package objects that point to anonymous arguments groups
-extern char** _cream_anonymous_relocated_args;                  //Extracts the buffer information to generate
+extern size_t _cream_anon_arg_count;                            // Contains the total number of anonymous arguments
+extern _expandable_stack _cream_anonymous_args;                 // Contains _cream_data_package objects that point to anonymous arguments groups
+extern char** _cream_anonymous_relocated_args;                  // Extracts the buffer information to generate
