3de10d86482346d761ffea99b2ddd1a7d2365b03
JuanmaDevG <juanmagonzalezmartinez@protonmail.com>
2026-03-09 23:21:46 +0100

    The structure has changed to a generic table model, and so functions have been rebuilt

--- TODO.md
+++ TODO.md
@@ -8,6 +8,7 @@ Must be compile-time, so the library will be:
 - will be sequential
 - will support subcommand contexts (composition)
 - rewrite the god damn readme
+- will have a way to deal with anonymous arguments
 
 I might delete this project if I don't consider it useful enough
 
--- src/cream_craft.h
+++ src/cream_craft.h
@@ -12,85 +12,118 @@
 #define CREAM_DEFAULT_SLOTS 20
 #define CREAM_DEFAULT_TXT_LENGTH 4098
 
+#define CREAM_ARG_NEEDS_DATA 0x01
+#define CREAM_ARG_IS_MANDATORY 0x02
+#define _CREAM_ARG_HAS_CHECKS 0x04
+
+typedef struct {
+  size_t count;
+  void* base;
+  void* next;
+  void* limit;
+} _cream_buf;
+
 typedef struct cream_arg cream_arg;
 
 typedef struct {
-  unsigned short _arg_length;
-  unsigned char* _data;
-  bool _mandatory;
+  unsigned short length;
+  unsigned char* data;
+  unsigned char flags;
 } _cream_arg;
 
 typedef struct {
   _cream_arg* parent;
   _cream_arg* child;
-} cream_relationship;
+} _cream_relationship;
 
-static size_t _cream_args_cap = 0;
-static struct _cream_arg* _cream_arg_block = NULL;
-static struct _cream_arg* _cream_next_arg = NULL;
-static const struct _cream_arg* _cream_arg_block_limit = NULL;
-
-static size_t _cream_txt_size = 0;
-static unsigned char* _cream_txt_block = NULL;
-static unsigned char* _cream_next_string = NULL;
-static const unsigned char* _cream_txt_block_limit = NULL;
-
-static size_t _cream_rel_count = 0;
-static cream_relationship* _cream_rel_table = NULL;
+typedef struct {
+  unsigned short max_args;
+  unsigned short min_args;
+  const unsigned char* available_values; //NULL means anything
+} _cream_runtime_check;
 
 
-bool _cream_check_argtables(const size_t _new_text_size = 0)
+struct _cream_buf _cream_args = {0};
+struct _cream_buf _cream_text = {0};
+struct _cream_buf _cream_rels = {0};
+struct _cream_buf _cream_checks = {0};
+
+
+bool _cream_check_table(_cream_table* _tb, const size_t _min_available, const size_t _increment = 0, const bool _zalloc = false)
 {
-  if(_cream_next_arg == _cream_arg_block_limit)
-  {
-    _cream_args_cap += CREAM_DEFAULT_SLOTS;
-    _cream_arg_block = realloc(_cream_arg_block, _cream_args_cap);
-    //TODO: recalculate all pointers
-  }
-  if(_cream_next_string == _cream_txt_block_limit
-      || (_cream_txt_block_limit - _cream_next_string) < _new_text_size)
-  {
-  }
+  if((size_t)((char*)_tb->limit - (char*)_tb->next) >= _min_available)
+    return true;
+
+  off_t _next_idx = (char*)_tb->next - (char*)_tb->base;
+  _tb->count += (_increment > 0 ? _increment : _min_available);
+  _tb->base = realloc(_tb->base, _tb->count);
+  if(!_tb->base) return false;
+  _tb->next = ((char*)_tb->base) + _next_idx;
+  _tb->limit = ((char*)_tb->base) + _tb->count;
+  if(_zalloc)
+      memset(_tb->next, 0, (unsigned char*)_tb->limit - (unsigned char*)_tb->next);
   return true;
 }
 
-bool _cream_check_reltables()
+
+struct _cream_arg* _cream_find_arg(const char *const _arg)
 {
 }
 
 
-void cream_init()
+bool cream_craft_arg(const char *const _cream_new_arg, const unsigned char _flags = 0x00)
 {
-  _cream_args_cap = CREAM_DEFAULT_SLOTS;
-  _cream_arg_block = (struct cream_arg*)calloc(sizeof(cream_arg) * CREAM_DEFAULT_SLOTS);
-  _cream_next_arg = _cream_arg_block;
-  _cream_arg_block_limit = _cream_arg_block + _cream_args_cap;
-  _cream_txt_size = CREAM_DEFAULT_TXT_LENGTH;
-  _cream_txt_block = (unsigned char*)malloc(CREAM_DEFAULT_TXT_LENGTH);
-  _cream_next_string = _cream_txt_block;
-  _cream_txt_block_limit = _cream_txt_block + _cream_txt_size;
-}
-
-bool cream_make_arg(const char *const _cream_new_arg)
-{
-  if(!_cream_arg_block)
+  if(!_cream_new_arg) return false;
+  size_t _arg_len = strnlen(_cream_new_arg, CREAM_MAX_ARG_LENGTH);
+  if(!(_cream_check_table(&_cream_args, sizeof(_cream_arg), sizeof(_cream_arg) * CREAM_DEFAULT_SLOTS, true)
+        && _cream_check_table(&_cream_text, _arg_len, CREAM_DEFAULT_TEXT_LENGTH)
+        && _arg_len > 0))
+  {
     return false;
-
-  size_t arg_len = strnlen(_cream_new_arg, CREAM_MAX_ARG_LENGTH);
-  cream_check_argtables(arg_len);
-  _cream_next_arg->_arg_length = arg_len;
+  }
+  _cream_arg* _p = (struct _cream_arg*)_cream_args->next;
+  _p->length = _arg_len;
+  _p->data = (unsigned char*)_cream_text->next;
+  _p->flags = _flags;
+  memcpy(_p->data, _cream_new_arg, _arg_len);
+  (char*)_cream_text->next += _arg_len;
+  _p++;
+  return true;
 }
 
 
-//TODO: first version of this will manage children with simple allocations
-//  Later may be more improvements after measuring
-void cream_make_subarg(const char *const _cream_parent, const char *const _cream_child)
+bool cream_craft_subarg(const char *const _parent, const char *const _child, const unsigned char _flags = 0x00)
+{
+  struct _cream_arg *_parent_info, *_child_info;
+  _parent_info = _cream_find_arg(_parent);
+  if(!_cream_arg) return false;
+  _child_info = _cream_find_arg(_child);
+  if(!_child_info)
+  {
+    if(!cream_craft_arg(_child, _flags)) return false;
+    _child_info = _cream_find_arg(_child);
+  }
+  if(!_cream_check_table(&_cream_rels, sizeof(_cream_reloationship), sizeof(_cream_relationship) * CREAM_DEFAULT_SLOTS, true))
+    return false;
+  struct _cream_relationship* _r = (_cream_relationship*)_cream_rels->next;
+  _r->parent = _parent_info;
+  _r->child = _child_info;
+  _r++;
+  _cream_rels.next = _r;
+  return true;
+}
+
+
+bool cream_craft_runtime_check(const char *const _arg) //More param with future available checks
+{
+  return true;
+}
+
+
+bool cream_export(const char *const out_filename = "cream.args")
 {
 }
 
+//struct sometype* cream_compact() instead of writing to file (for a compile-time executable), compact to an in-memory readable format for cream
 
-bool cream_write(const char *const out_filename = "cream.args")
-{
-}
-
-#endif
+#endif // CREAMLIB_CRAFT_H
