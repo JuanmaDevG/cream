2a6002d984d38ee96fab3d1c25abc16ae9f388c6
JuanmaDevG <juanmagonzalezmartinez@protonmail.com>
2026-04-08 01:59:47 +0200

    I got into more engineering problems but they will be easily solved

--- cream.h
+++ cream.h
@@ -2,6 +2,7 @@
 #define CREAMLIB_H
 
 #include <cstdlib>
+#include <memory.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -74,7 +75,7 @@ union cream_argtype {
 
 struct cream_arg_opt {
   unsigned char flags;
-  const unsigned char *text;
+  const char *text;
   union cream_type_info info;
   void (*run_oncheck)(const union cream_argtype);
 };
@@ -87,47 +88,115 @@ struct cream_result {
   struct cream_bool *bools_end;
   struct cream_datavec *datavecs;
   struct cream_datavec *datavecs_end;
-  struct cream_subcommand *subcommands;
-  struct cream_subcommand *subcommands_end;
   struct cream_kwhost *kwhosts;
   struct cream_kwhost *kwhosts_end;
-  const unsigned char data;
+  struct cream_subcommand *subcommands;
+  struct cream_subcommand *subcommands_end;
+  unsigned char data[];
 };
 
-cream_result *cream_parse(const int argc, const char *argv[],
-                          const cream_arg_opt *_arg_options) {
-  unsigned short bools_count, datavecs_count, subcommands_count, kwhosts_count;
-  bools_count = datavecs_count = subcommands_count, kwhosts_count = 0;
-  for (const cream_arg_opt *_i = _arg_options; _i->text != NULL; _i++) {
+void _count_types(cream_arg_opt *_opts, unsigned int *_bools,
+                  unsigned int *_datavecs, unsigned int *_kwhosts,
+                  unsigned int *_subcommands) {
+  *_bools = *_datavecs = *_kwhosts = *_subcommands = 0;
+  for (const cream_arg_opt *_i = _opts; _i->text != NULL; _i++) {
     switch (_i->flags & 0x03) {
     case CREAM_TYPE_BOOLEAN:
-      bools_count++;
+      (*_bools)++;
       break;
     case CREAM_TYPE_DATAVEC:
-      datavecs_count++;
+      (*_datavecs)++;
       break;
     case CREAM_TYPE_KEYWORD_HOST:
-      kwhosts_count++;
+      (*_kwhosts)++;
       break;
     case CREAM_TYPE_SUBCOMMAND:
-      subcommands_count++;
+      (*_subcommands)++;
       break;
     }
   }
+}
+
+bool _streq(const char *_s1, const char *_s2) {
+  for (int i = 0; _s1[i] != '\0' && _s2[i] != '\0'; i++) {
+    if (_s1[i] != _s2[i])
+      return false;
+  }
+  return true;
+}
+
+void _register_argument(const int _rem_argc, const char **_rem_argv,
+                        const cream_arg_opt *_opt, cream_result *_res) {
+  switch (_opt->flags & 0x03) {
+  case CREAM_TYPE_BOOLEAN:
+    break;
+  case CREAM_TYPE_DATAVEC:
+    break;
+  case CREAM_TYPE_KEYWORD_HOST:
+    break;
+  case CREAM_TYPE_SUBCOMMAND:
+    break;
+  }
+}
+
+cream_result *cream_parse(const int argc, const char *argv[],
+                          const cream_arg_opt *_arg_options) {
+  unsigned int bools_count, datavecs_count, subcommands_count, kwhosts_count,
+      anon_args_count;
+  _count_types(_arg_options, &bools_count, &datavecs_count, &kwhosts_count,
+               &subcommands_count, &anon_args_count);
 
   cream_result *res = (cream_result *)malloc(
       sizeof(cream_result) + (sizeof(cream_bool) * bools_count) +
       (sizeof(cream_datavec) * datavecs_count) +
       (sizeof(cream_subcommand) * subcommands_count) +
-      (sizeof(cream_kwhost) * kwhosts_count));
+      (sizeof(cream_kwhost) * kwhosts_count) +
+      (sizeof(char *) * anon_args_count));
 
   if (!res) {
     perror("There is not enough memory to start argument parsing");
     return NULL;
   }
 
-  for (int i = 0; i < argc; i++) {
-    // TODO: parsing loop
+  memset(res, 0, sizeof(cream_result));
+
+  // TODO: create a macro to automate field filling (and clean up counters)
+  res->bools = (cream_bool *)res->data;
+  res->bools_end = res->bools + bools_count;
+  if (res->bools == res->bools_end)
+    res->bools = NULL;
+  res->datavecs = (cream_datavec *)res->bools_end;
+  res->datavecs_end = res->datavecs + datavecs_count;
+  if (res->datavecs == res->datavecs_end)
+    res->datavecs = NULL;
+  res->kwhosts = (cream_kwhost *)res->datavecs_end;
+  res->kwhosts_end = res->kwhosts + kwhosts_count;
+  if (res->kwhosts == res->kwhosts_end)
+    res->kwhosts = NULL;
+  res->subcommands = (cream_subcommand *)res->kwhosts_end;
+  res->subcommands_end = res->subcommands + subcommands_count;
+  if (res->subcommands == res->subcommands_end)
+    res->subcommands = NULL;
+  res->anonymous_args = (const char **)res->subcommands_end;
+  res->anonymous_args_end = res->anonymous_args + anon_args_count;
+  if (res->anonymous_args == res->anonymous_args_end)
+    res->anonymous_args = NULL;
+
+  for (int _i = 0; _i < argc; _i++) {
+    const char *cur_arg = argv[_i];
+    bool is_anon = false;
+    for (const cream_arg_opt *_opt = _arg_options; _opt->text != NULL; _opt++) {
+      // TODO: watch out keyword hosts
+      if (!_streq((const char *)cur_arg, _opt->text))
+        continue;
+      // Is an option, receive info
+      _register_argument(argc - _i, argv + _i, _opt, res);
+      is_anon = true;
+      break;
+    }
+    if (is_anon) {
+      // TODO: register as anonymous argument
+    }
   }
 
   return res;
